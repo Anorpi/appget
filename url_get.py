2c4abce1382780a798af1f39c1b04384ca9a9839
Anorpi <5262122@gmail.com>
2016-06-17 10:42:36 +0800

    add get Everything-x64-exe-Multilingual download url

--- url_get.py
+++ url_get.py
@@ -6,3 +6,10 @@ python_soup = BeautifulSoup(python_web.read(),"lxml")
 tags_a =python_soup.findAll(name="a",attrs={'href':re.compile("^https?://.*python\-2.*(exe|msi|zip|rar)$")})
 for tag_a in tags_a:
 	print tag_a["href"]
+
+
+Everything_web =urllib.urlopen("https://www.voidtools.com/downloads/")
+Everything_soup = BeautifulSoup(Everything_web.read(),"lxml")
+tags_a =Everything_soup.findAll(name="a",attrs={'href':re.compile("^\/Everything\-.*[0-9]\.x64.Multilingual-Setup.exe$")})
+for tag_a in tags_a:
+	print "https://www.voidtools.com%s" % tag_a["href"]
