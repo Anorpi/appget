ffd054ea374a43cdd52cd24a577d7f2aadd96d14
Anorpi <5262122@gmail.com>
2016-06-18 00:34:30 +0800

    add some download url

--- url_get.py
+++ url_get.py
@@ -1,7 +1,13 @@
 from bs4 import BeautifulSoup
-import urllib
+import urllib,requests
 import re
 python_web =urllib.urlopen("https://www.python.org/downloads/")
+resp = requests.get("https://www.python.org/downloads/")
+if python_web == resp:
+	print "same"
+else:
+	print "not same"
+
 python_soup = BeautifulSoup(python_web.read(),"lxml")
 tags_a =python_soup.findAll(name="a",attrs={'href':re.compile("^https?://.*python\-2.*(exe|msi|zip|rar)$")})
 for tag_a in tags_a:
@@ -13,3 +19,27 @@ Everything_soup = BeautifulSoup(Everything_web.read(),"lxml")
 tags_a =Everything_soup.findAll(name="a",attrs={'href':re.compile("^\/Everything\-.*[0-9]\.x64.Multilingual-Setup.exe$")})
 for tag_a in tags_a:
 	print "https://www.voidtools.com%s" % tag_a["href"]
+
+nmap_web =urllib.urlopen("https://nmap.org/download.html")
+nmap_soup = BeautifulSoup(nmap_web.read(),"lxml")
+tags_a =nmap_soup.findAll(name="a",attrs={'href':re.compile("^.*setup\.(exe|msi|zip|rar)$")})
+for tag_a in tags_a:
+	print tag_a["href"]
+
+
+winscp_web =urllib.urlopen("https://winscp.net/eng/download.php")
+winscp_soup = BeautifulSoup(winscp_web.read(),"lxml")
+tags_a =winscp_soup.findAll(name="a",attrs={'href':re.compile("^.*setup\.(exe|msi|zip|rar)$")})
+for tag_a in tags_a:
+	print "https://winscp.net%s" % tag_a["href"][2:]
+	#https://winscp.net/ ../download/winscp577setup.exe
+
+
+
+#https://www.jetbrains.com/pycharm/download/#section=windows
+#https://download.jetbrains.com/python/pycharm-community-2016.1.4.exe
+#nmap_web =urllib.urlopen("https://nmap.org/download.html")
+#nmap_soup = BeautifulSoup(nmap_web.read(),"lxml")
+#tags_a =nmap_soup.findAll(name="a",attrs={'href':re.compile("^.*(exe|msi|zip|rar)$")})
+#for tag_a in tags_a:
+#	print "https://www.voidtools.com%s" % tag_a["href"]
